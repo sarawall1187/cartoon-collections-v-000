160034f028e04855661e9adb1034cec6b296bfc6
Sara Walker <sarawall1187@gmail.com>
2018-12-16 20:44:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients.detect do |cheese|
-    binding.pry
+   
     ingredients.include?(cheese_types) 
   "#{cheese}"
      end  
