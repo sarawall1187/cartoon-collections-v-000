e40ce6eb1060edf7cf1a26cab1944b2792e15c14
Sara Walker <sarawall1187@gmail.com>
2018-12-16 20:19:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.each.detect { |cheese| puts "#{cheese}"
+  ingredients.each.detect { |cheese| puts "#{cheese}"}
 end    
   
 #iterate over a given string to check if cheese type is included
